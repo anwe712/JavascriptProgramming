5a606132d2a79e1b10d0d8bf769bf7ba7c29e4ae
anwe712 <ghoshanwesha272@gmail.com>
2024-04-21 12:21:33 +0530

    code added

---
+++ 0Basics/let,var,const.js
@@ -0,0 +1,9 @@
+const accountID = 1293729 ;
+let accountEmail = "ghoshanwesha272@gmail.com" ;
+
+var password = "password123" ;
+
+accountCity = "Lucknow" ;
+AccountIP = 122839393 ;
+
+console.table ([accountEmail , accountID,password , accountCity]) ;
---
+++ 0Basics/variables.js
@@ -0,0 +1,34 @@
+// Variables and Data Types
+let name = "John";
+let age = 30;
+const PI = 3.14;
+let isStudent = true;
+let fruits = ["apple", "banana", "orange"];
+let person = { firstName: "John", lastName: "Doe" };
+
+// Operators
+let x = 5;
+let y = 3;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
+
+// Basic Control Flow Statements
+if (age >= 18) {
+    console.log(name + " is an adult.");
+} else {
+    console.log(name + " is a minor.");
+}
+
+let i = 0;
+while (i < 5) {
+    console.log("While loop iteration: " + i);
+    i++;
+}
+
+for (let j = 0; j < fruits.length; j++) {
+    console.log("Fruit: " + fruits[j]);
+}
+
